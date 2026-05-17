acc481650da43368aeda3568f612ece54bb2ad63
Gagneet kaur <gagneetkaur23@gmail.com>
2026-05-17 16:47:27 +0530

    string comparison

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Java");
+
+        sb.deleteCharAt(1);
+
+        System.out.println(sb);
+    }
+}
